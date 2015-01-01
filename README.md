f5974bce3776bb51009c06609fb6a7e8401d2a86
Roark Brewster <roark@DJ-Schwends-MacBook-2.local>
2015-01-01 09:23:57 -0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 muscle_tracker
 ==============
+
+start with "rackup"
+muscle tracker live at:
+http://localhost:9292/dashboard
+
+good Sinatra + Haml tutorial:
+http://code.tutsplus.com/tutorials/an-introduction-to-haml-and-sinatra--net-14858
+
+getting Sinatra working with Rack:
+https://www.digitalocean.com/community/tutorials/how-to-install-and-get-started-with-sinatra-on-your-system-or-vps
+
+ruby + android:
+https://github.com/ruboto/ruboto/blob/master/README.md
+
+using a yaml database
+http://www.techrepublic.com/blog/software-engineer/simple-data-storage-with-ruby/
--- muscle_tracker.rb
+++ muscle_tracker.rb
@@ -1,21 +1,5 @@
 #!/usr/bin/env ruby
 
-# start with "rackup"
-# muscle tracker live at:
-# http://localhost:9292/dashboard
-#
-# good Sinatra + Haml tutorial:
-# http://code.tutsplus.com/tutorials/an-introduction-to-haml-and-sinatra--net-14858
-# 
-# getting Sinatra working with Rack:
-# https://www.digitalocean.com/community/tutorials/how-to-install-and-get-started-with-sinatra-on-your-system-or-vps
-#
-# ruby + android:
-# https://github.com/ruboto/ruboto/blob/master/README.md
-# 
-# using a yaml database
-# http://www.techrepublic.com/blog/software-engineer/simple-data-storage-with-ruby/
-
 require 'rubygems'
 require 'sinatra'
 require 'haml'

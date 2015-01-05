dfa158905f26932da31d2bc1dbe12dd2a57d2c90
roark <roarkb@gmail.com>
2015-01-05 12:39:17 -0700

    unify UX to single page

--- muscle_tracker.rb
+++ muscle_tracker.rb
@@ -23,25 +23,13 @@ def date_to_datestamp(date)
   Date.new(d[0].to_i, d[1].to_i, d[2].to_i)
 end
 
-before do
-  DB.append_latest_dates
-  #DB.update_value_array("2014-12-31", [ "chest", "back", "abs", "shoulders", "deltoids", "biceps", "triceps", "legs", "cardio" ])
-end
-
 get '/' do
- redirect to('/dashboard')
-end
-
-get '/dashboard' do
-  haml :dashboard, :format => :xhtml
-end
-
-get '/calendar' do
+  DB.append_latest_dates
   
   events = []
-  DB.select_all.sort.each do |e|
+  DB.select_all.sort.reverse.each do |e|
     events.push([ pretty_date(date_to_datestamp(e[0])), e[1].join(", ") ])
   end
-
-  haml :calendar, :format => :xhtml, :locals => { :events => events }
+  
+  haml :main, :format => :xhtml, :locals => { :events => events }
 end
